5e2c3d478a60ce111e168b30345b1d25c4b98012
Victor <98230507+VictorCTM@users.noreply.github.com>
2023-03-31 08:37:07 +0200

    Add files via upload

---
+++ js/show.js
@@ -0,0 +1,141 @@
+var div1 = document.getElementById('fonc-1');
+var div2 = document.getElementById('fonc-2');
+var div3 = document.getElementById('fonc-3');
+var div4 = document.getElementById('fonc-4');
+var div5 = document.getElementById('fonc-5');
+var div6 = document.getElementById('fonc-6');
+var div7 = document.getElementById('fonc-7');
+var div8 = document.getElementById('fonc-8');
+var div9 = document.getElementById('fonc-9');
+var div10 = document.getElementById('fonc-10');
+
+var display1 = 0;
+var display2 = 0;
+var display3 = 0;
+var display4 = 0;
+var display5 = 0;
+var display6 = 0;
+var display7 = 0;
+var display8 = 0;
+var display9 = 0;
+var display10 = 0;
+
+
+function show1(){
+	//display 1
+	if(display1 == 1){ //si la div est visible on la rend invisible
+		div1.style.display = 'none';
+		display1 = 0;
+	}else{ //sinon on la rend visible
+		div1.style.display = 'flex';
+		display1 = 1;
+	}
+}
+
+
+function show2(){
+	//display 2
+	if(display2 == 1){ //si la div est visible on la rend invisible
+		div2.style.display = 'none';
+		display2 = 0;
+	}else{ //sinon on la rend visible
+		div2.style.display = 'flex';
+		display2 = 1;
+	}
+}
+
+
+function show3(){
+	//display 3
+	if(display3 == 1){ //si la div est visible on la rend invisible
+		div3.style.display = 'none';
+		display3 = 0;
+	}else{ //sinon on la rend visible
+		div3.style.display = 'flex';
+		display3 = 1;
+	}
+}
+
+
+function show4(){
+	//display 4
+	if(display4 == 1){ //si la div est visible on la rend invisible
+		div4.style.display = 'none';
+		display4 = 0;
+	}else{ //sinon on la rend visible
+		div4.style.display = 'flex';
+		display4 = 1;
+	}
+}
+
+
+function show5(){
+	//display 5
+	if(display5 == 1){ //si la div est visible on la rend invisible
+		div5.style.display = 'none';
+		display5 = 0;
+	}else{ //sinon on la rend visible
+		div5.style.display = 'flex';
+		display5 = 1;
+	}
+}
+
+
+function show6(){
+	//display 6
+	if(display6 == 1){ //si la div est visible on la rend invisible
+		div6.style.display = 'none';
+		display6 = 0;
+	}else{ //sinon on la rend visible
+		div6.style.display = 'flex';
+		display6 = 1;
+	}
+}
+
+
+function show7(){
+	//display 7
+	if(display7 == 1){ //si la div est visible on la rend invisible
+		div7.style.display = 'none';
+		display7 = 0;
+	}else{ //sinon on la rend visible
+		div7.style.display = 'flex';
+		display7 = 1;
+	}
+}
+
+
+function show8(){
+	//display 8
+	if(display8 == 1){ //si la div est visible on la rend invisible
+		div8.style.display = 'none';
+		display8 = 0;
+	}else{ //sinon on la rend visible
+		div8.style.display = 'flex';
+		display8 = 1;
+	}
+}
+
+
+function show9(){
+	//display 9
+	if(display9 == 1){ //si la div est visible on la rend invisible
+		div9.style.display = 'none';
+		display9 = 0;
+	}else{ //sinon on la rend visible
+		div9.style.display = 'flex';
+		display9 = 1;
+	}
+}
+
+
+function show10(){
+	//display 10
+	if(display10 == 1){ //si la div est visible on la rend invisible
+		div10.style.display = 'none';
+		display10 = 0;
+	}else{ //sinon on la rend visible
+		div10.style.display = 'flex';
+		display10 = 1;
+	}
+}
